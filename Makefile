71a6c90da77af781647d68f72ed0b1d2067b26af
Barzabel <kochenko1995@mail.ru>
2022-10-03 19:44:57 +0300

    git ignore fix

--- Makefile
+++ Makefile
@@ -7,3 +7,6 @@ console:
 migrate:
 	poetry run python manage.py makemigrations
 	poetry run python manage.py migrate
+
+run:
+	poetry run python manage.py runserver
--- todo/settings.py
+++ todo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from .secrets import KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!@qoh(8v6phn=w-!0h^y#mqi2_1m4yupy60l-te8_(k#dns_@c'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
