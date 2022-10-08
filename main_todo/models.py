1dda576f5b557c44d7bb4cc2be6ff759e95d1a34
Barzabel <kochenko1995@mail.ru>
2022-10-08 12:17:56 +0300

    add model todo

--- main_todo/models.py
+++ main_todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+
+class Todo(models.Model):
+    titel = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    date_done = models.DateTimeField(null=True)
+    memo = models.TextField(blank=True)
+    imported = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return u"{}".format(self.titel)
