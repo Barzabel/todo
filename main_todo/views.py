342c9cfe8b7d665f2ee40ac45033553f5844890e
Barzabel <kochenko1995@mail.ru>
2022-10-26 20:19:19 +0300

    add view todo

--- main_todo/views.py
+++ main_todo/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User 
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from django.utils import timezone
 from .forms import TodoForm
 from .models import Todo
 
@@ -66,3 +67,29 @@ def createtodo(request):
 def home(request):
     return render(request, 'main_todo/home.html', {})
 # Create your views here.
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'main_todo/viewtodo.html', {"todo":todo, "form":form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('current_todo')
+        except ValueError:
+            return render(request, 'main_todo/viewtodo.html', {"todo":todo, "form":form, 'error': "не верные данные" })
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.date_done = timezone.now()
+        todo.save()
+        return redirect('current_todo')
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('current_todo')
